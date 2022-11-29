9f9ed6114f5642fb90c047fab4c0d549d45b27bf
Guilherme Silva Carpi <62569003+GuilhermeSilvaCarpi@users.noreply.github.com>
2022-11-29 20:46:44 -0300

    string.format()
    aula do w3schools sobre o método format.

--- treinamento progrmação/Python/w3schools/4.Strings/9.String formatting.py
+++ treinamento progrmação/Python/w3schools/4.Strings/9.String formatting.py
@@ -1,1 +1,14 @@
-print('somethink')
+# metodo format
+firstname = 'Guilerme'
+lastname = 'Carpi'
+print('nome: {}'.format('{} {}'.format(firstname, lastname)))
+
+# indexs numerados
+complitename = '{0} {1}'.format(firstname, lastname)
+
+age = 17
+print('conhece {0}? {0} tem {1} anos de idade'.format(firstname, age))
+
+# indexs nomeados
+print('eu vou querer um {entrada}, depois um {pratoprincipal}, e no final {sobremesa}'.format(
+entrada = 'pastel', pratoprincipal = 'arroz com feijão', sobremesa = 'sorvete'))
